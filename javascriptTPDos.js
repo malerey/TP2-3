c65f11809f61137d62174b72f47abfbb8d074395
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-03-04 12:23:53 -0300

    M

---
+++ javascriptTPDos.js
@@ -0,0 +1,50 @@
+//console.log('Hola Mundo')
+
+//Local de ventas de PCs
+//Una empresa de venta de computadoras está desarrollando un sistema para llevar registro de ventas. Para ello cuenta con la siguiente información:
+
+//Lista de las vendedoras de la empresa
+//Lista de ventas. Un array con objetos. Cada objeto representa una venta y tiene las propiedades fecha, nombreVendedora (un String con el nombre), componentes (un array Strings con el nombre de cada componente vendido).
+//Lista de precios de los componentes, de la forma (nombre componente, precio).
+
+var local = {
+  vendedoras: ["Ada", "Grace", "Hedy", "Sheryl"],
+
+  ventas: [
+    { fecha: new Date(2019, 1, 4), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000 (1)", "Motherboard ASUS 1500(2)"] },
+    { fecha: new Date(2019, 0, 1), nombreVendedora: "Ada", componentes: ["Monitor GPRS 3000(3)", "Motherboard ASUS 1500(4)"] },
+    { fecha: new Date(2019, 0, 2), nombreVendedora: "Grace", componentes: ["Monitor ASC 543(5)", "Motherboard MZI(6)"] },
+    { fecha: new Date(2019, 0, 10), nombreVendedora: "Ada", componentes: ["Monitor ASC 543(7)", "Motherboard ASUS 1200(8)"] },
+    { fecha: new Date(2019, 0, 12), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000(9)", "Motherboard ASUS 1200(10)"] }
+  ],
+
+  precios: [
+    { componente: "Monitor GPRS 3000", precio: 200 },
+    { componente: "Motherboard ASUS 1500", precio: 120 },
+    { componente: "Monitor ASC 543", precio: 250 },
+    { componente: "Motherboard ASUS 1200", precio: 100 },
+    { componente: "Motherboard MZI", precio: 30 },
+    { componente: "HDD Toyiva", precio: 90 },
+    { componente: "HDD Wezter Dishital", precio: 75 },
+    { componente: "RAM Quinston", precio: 110 },
+    { componente: "RAM Quinston Fury", precio: 230 }
+  ]
+};
+
+var cadaComponente = local.ventas.forEach(function(venta){
+    for(i=0;i<venta.componentes.length;i++){
+
+        return venta.componentes[i]
+    }
+})
+
+console.log(cadaComponente)
+
+
+
+
+//Se pide desarrollar las siguientes funciones:
+
+//precioMaquina(componentes): recibe un array de componentes y devuelve el precio de la máquina que se puede armar con esos componentes, que es la suma de los precios de cada componente incluido.
+
+//console.log( precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]) ); // 320 ($200 del monitor + $120 del motherboard)
